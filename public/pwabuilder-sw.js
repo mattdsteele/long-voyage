be4f26443cb7ecc00e4b530ec28b916c2e4e8d8d
Matt Steele <orphum@gmail.com>
2021-08-09 12:49:07 -0500

    Update pwabuilder-sw.js

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -12,7 +12,7 @@ self.addEventListener("message", (event) => {
 
 workbox.routing.registerRoute(
   new RegExp('/*'),
-  new workbox.strategies.CacheFirst({
+  new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
 );
