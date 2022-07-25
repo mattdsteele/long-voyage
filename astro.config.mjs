c7aae548146aa1a21164f0aa99bc6a5305ff120f
mattdsteele <orphum@gmail.com>
2022-07-25 18:00:44 -0500

    Add GA via Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,6 +8,13 @@ export default defineConfig({
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  site: 'https://longvoyage.steele.blue',
-  integrations: [preact()]
+  site: "https://longvoyage.steele.blue",
+  integrations: [
+    preact(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
